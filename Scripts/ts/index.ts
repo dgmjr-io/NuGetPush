824256d37d2239aefea11596939f93a369e94846
David G. Moore, Jr <david@dgmjr.io>
2023-10-07 17:20:12 -0400

    reworking this to be a npm package

---
+++ Scripts/ts/index.ts
@@ -0,0 +1,8 @@
+import * as constants from './constants';
+export { constants };
+import * as delete_github_package_version from './delete-github-package-version'
+export { delete_github_package_version };
+import * as github_cli_types from './github-cli-types';
+export { github_cli_types };
+import * as github_cli from './github-cli';
+export { github_cli };
